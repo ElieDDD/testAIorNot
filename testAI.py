f868d8cb509ce4f7398bce8efbe0d049c61b230c
ElieDDD <30892423+ElieDDD@users.noreply.github.com>
2025-01-06 11:28:19 +0000

    Update testAI.py

--- testAI.py
+++ testAI.py
@@ -1,4 +1,3 @@
-
 import streamlit as st 
 import os 
 from dotenv import load_dotenv
@@ -8,15 +7,15 @@ from openai import OpenAI
 
 load_dotenv()
 key = os.getenv('OPEN_AI_KEY')
-MODEL = 'gpt-4o'https://github.com/ElieDDD
+MODEL = 'gpt-4o'
 api_key = st.secrets["OPENAI_API_KEY"]
 client = OpenAI(api_key = key)
 
 def encode_image(image):
     return base64.b64encode(image.read()).decode('utf-8')
 
-st.title('AI generated or not?')
-image_file = st.file_uploader('Upload an image to test',type = ['png', 'jpg', 'jpeg'])
+st.title('Listening to training images')
+image_file = st.file_uploader('Upload an image',type = ['png', 'jpg', 'jpeg'])
 if image_file:
     st.image(image_file,caption = 'image')
 
@@ -27,7 +26,7 @@ if image_file:
     messages=[
         {"role": "system", "content": "You are a helpful  assistant that responds in Markdown."},
         {"role": "user", "content": [
-        {"type": "text", "text": "Do you think this is an Ai generated image or not? Tell us how you make this evaluation and give us a confidence score for your evaluation."},
+        {"type": "text", "text": "Could you describe the image and create a narrative to highlight important details and provide recommendations. Create a narrative with Observations, important details, and ideas for further analysis with bullet points."},
             {"type": "image_url", "image_url": {
             "url": f"data:image/png;base64,{base64_image}"}
                     }
