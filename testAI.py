4f47002725dd740663a4f1aa91699d7e5726dfcb
ElieDDD <30892423+ElieDDD@users.noreply.github.com>
2025-01-06 11:25:43 +0000

    Update testAI.py

--- testAI.py
+++ testAI.py
@@ -1,27 +1,24 @@
+
 import streamlit as st 
 import os 
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 import base64
 from openai import OpenAI
-from PIL import Image
+
 
 load_dotenv()
 key = os.getenv('OPEN_AI_KEY')
-MODEL = 'gpt-4o'
+MODEL = 'gpt-4o'https://github.com/ElieDDD
 api_key = st.secrets["OPENAI_API_KEY"]
 client = OpenAI(api_key = key)
 
 def encode_image(image):
     return base64.b64encode(image.read()).decode('utf-8')
 
-st.title('Image Analyzer')
-#image_file = st.file_uploader('Upload an image file',type = ['png', 'jpg', 'jpeg'])
-
-image_file = Image.open('mug.png')
-
-#st.image_file(image, caption='Sunrise by the mountains')
+st.title('AI generated or not?')
+image_file = st.file_uploader('Upload an image to test',type = ['png', 'jpg', 'jpeg'])
 if image_file:
-    st.image(image_file,caption = 'Uploaded image',use_column_width =True)
+    st.image(image_file,caption = 'image')
 
     base64_image = encode_image(image_file)
 
@@ -30,7 +27,7 @@ if image_file:
     messages=[
         {"role": "system", "content": "You are a helpful  assistant that responds in Markdown."},
         {"role": "user", "content": [
-        {"type": "text", "text": "Could you describe the image and create a report to highlight important details and provide recommendations. Create a report with Observations, important details, and Recommendations with bullet points."},
+        {"type": "text", "text": "Do you think this is an Ai generated image or not? Tell us how you make this evaluation and give us a confidence score for your evaluation."},
             {"type": "image_url", "image_url": {
             "url": f"data:image/png;base64,{base64_image}"}
                     }
